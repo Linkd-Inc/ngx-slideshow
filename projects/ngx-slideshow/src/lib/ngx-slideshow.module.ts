5f1b23b1f0d7d46a3547e6cab2413121304fea37
Corbin <corbin@wireflare.com>
2018-05-10 13:18:45 -0700

    fix(ie): Added fixes for IE in order to properly display
    BREAKING CHANGE: This release upgrades to Angular 6. Angular 4-5 support has been dropped

---
+++ projects/ngx-slideshow/src/lib/ngx-slideshow.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxSlideshowComponent } from './ngx-slideshow.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [NgxSlideshowComponent],
+  exports: [NgxSlideshowComponent]
+})
+export class NgxSlideshowModule { }
---
+++ projects/ngx-slideshow/src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-slideshow
+ */
+
+export * from './lib/ngx-slideshow.component';
+export * from './lib/ngx-slideshow.module';
